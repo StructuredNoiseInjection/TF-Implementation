7e13b0ba6070faba3df8f670835e505deb787b24
StructuredNoiseInjection <60062116+StructuredNoiseInjection@users.noreply.github.com>
2020-02-05 15:12:05 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -10,7 +10,7 @@
 #----------------------------------------------------------------------------
 # Paths.
 
-result_dir = '/scratch/dragon/intel/alharbya/deleteLater'
+result_dir = 'results'
 data_dir = 'datasets'
 cache_dir = 'cache'
 run_dir_ignore = ['results', 'datasets', 'cache']
